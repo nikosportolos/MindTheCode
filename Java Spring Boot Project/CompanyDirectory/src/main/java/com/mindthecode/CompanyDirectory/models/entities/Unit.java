8cae8d62b5fa4242b998e4da941206fb7c8da43a
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:09:02 +0200

    Implemented Unit entity

---
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/models/entities/Unit.java
@@ -0,0 +1,62 @@
+package com.mindthecode.CompanyDirectory.models.entities;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Units")
+public class Unit {
+
+    /**
+     * Instance variables
+     **/
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    @ManyToOne
+    private Department department;
+
+    /**
+     * Constructors
+     **/
+
+    public Unit() {
+    }
+
+    public Unit(String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    /**
+     * Getters / Setters
+     **/
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
