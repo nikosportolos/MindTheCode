3eaac4a8a86f25f70c9797afa3598d9cfa0b2796
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:09:54 +0200

    Implemented AllUnitsResponse

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/models/responses/AllUnitsResponse.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/models/responses/AllUnitsResponse.java
@@ -1,4 +1,20 @@
 package com.mindthecode.CompanyDirectory.models.responses;
 
+import java.util.List;
+
 public class AllUnitsResponse {
+
+    private List<UnitResponse> units;
+
+    public AllUnitsResponse(List<UnitResponse> units) {
+        this.units = units;
+    }
+
+    public List<UnitResponse> getUnits() {
+        return units;
+    }
+
+    public void setUnits(List<UnitResponse> units) {
+        this.units = units;
+    }
 }
