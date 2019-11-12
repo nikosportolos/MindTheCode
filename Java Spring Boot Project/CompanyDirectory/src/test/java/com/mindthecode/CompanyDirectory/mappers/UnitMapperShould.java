ecd891e791532b02bf40e43659a371a5f900d319
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:18:42 +0200

    Created unit tests for UnitMapper

--- Java Spring Boot Project/CompanyDirectory/src/test/java/com/mindthecode/CompanyDirectory/mappers/UnitMapperShould.java
+++ Java Spring Boot Project/CompanyDirectory/src/test/java/com/mindthecode/CompanyDirectory/mappers/UnitMapperShould.java
@@ -1,4 +1,29 @@
 package com.mindthecode.CompanyDirectory.mappers;
 
+import com.mindthecode.CompanyDirectory.models.entities.Department;
+import com.mindthecode.CompanyDirectory.models.entities.Unit;
+import com.mindthecode.CompanyDirectory.models.responses.UnitResponse;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
 public class UnitMapperShould {
+
+    private UnitMapper unitMapper;
+    private Unit unitInput;
+    private UnitResponse expectedOutput;
+
+    @Before()
+    public void setup() {
+        unitMapper = new UnitMapper();
+
+        expectedOutput = new UnitResponse(1, "", new Department());
+    }
+
+    @Test
+    public void mapUserToUserResponse() {
+        UnitResponse output = unitMapper.mapUnitToResponse(unitInput);
+        Assert.assertThat(expectedOutput, Matchers.samePropertyValuesAs(output));
+    }
 }
