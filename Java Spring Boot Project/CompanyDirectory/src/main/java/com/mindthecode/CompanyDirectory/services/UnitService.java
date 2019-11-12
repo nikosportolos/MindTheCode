d57b09cdfd8a5207f3ccd9dc398a071a04d395a1
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:11:21 +0200

    Implemented UnitService

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/services/UnitService.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/services/UnitService.java
@@ -1,4 +1,37 @@
 package com.mindthecode.CompanyDirectory.services;
 
+
+import com.mindthecode.CompanyDirectory.mappers.UnitMapper;
+import com.mindthecode.CompanyDirectory.models.entities.Unit;
+import com.mindthecode.CompanyDirectory.models.responses.AllUnitsResponse;
+import com.mindthecode.CompanyDirectory.models.responses.UnitResponse;
+import com.mindthecode.CompanyDirectory.repositories.UnitRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UnitService {
+
+    @Autowired
+    UnitRepo repo;
+
+    @Autowired
+    UnitMapper mapper;
+
+    public AllUnitsResponse getAllUnits() {
+        return new AllUnitsResponse(mapper.mapUnits(repo.findAll()));
+    }
+
+    public AllUnitsResponse getUnitById(long id) {
+        List<UnitResponse> positionUnits = new ArrayList<>();
+        Iterable<Unit> retrievedUnits = repo.findAll();
+        for (Unit position : retrievedUnits) {
+            if (position.getId() == id)
+                positionUnits.add(mapper.mapUnitToResponse(position));
+        }
+        return new AllUnitsResponse(positionUnits);
+    }
 }
