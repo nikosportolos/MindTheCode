99c8525fff607ec53233c23c0f274251258cd1f6
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:11:06 +0200

    Implemented UnitRepo

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/repositories/UnitRepo.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/repositories/UnitRepo.java
@@ -1,4 +1,32 @@
 package com.mindthecode.CompanyDirectory.repositories;
 
-public class UnitRepo {
+import com.mindthecode.CompanyDirectory.models.entities.Unit;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+public interface UnitRepo extends CrudRepository<Unit, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Unit> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Unit> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Unit entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Unit> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
