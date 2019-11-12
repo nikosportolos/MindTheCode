aa87cbb3701ec5b17b720ce6e8a4917d583ed43d
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-12 13:10:49 +0200

    Implemented UnitMapper

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/mappers/UnitMapper.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/mappers/UnitMapper.java
@@ -1,4 +1,24 @@
 package com.mindthecode.CompanyDirectory.mappers;
 
+import com.mindthecode.CompanyDirectory.models.entities.Unit;
+import com.mindthecode.CompanyDirectory.models.responses.UnitResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UnitMapper {
+
+    public List<UnitResponse> mapUnits(Iterable<Unit> all) {
+        List<UnitResponse> response = new ArrayList<>();
+        for (Unit unit : all) {
+            response.add(mapUnitToResponse(unit));
+        }
+        return response;
+    }
+
+    public UnitResponse mapUnitToResponse(Unit unit) {
+        return new UnitResponse(unit.getId(), unit.getName(), unit.getDepartment());
+    }
 }
