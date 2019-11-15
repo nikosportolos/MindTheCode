8afcb4f1951e3b3a047fe7e8c7ace9f717cc5de8
dimitrafountzoula <dimfou109@gmail>
2019-11-15 13:53:16 +0200

    Implementetion of BusinessMapperShould

---
+++ Java Spring Boot Project/CompanyDirectory/src/test/java/com/mindthecode/CompanyDirectory/BusinessUnitMapperShould.java
@@ -0,0 +1,37 @@
+package com.mindthecode.CompanyDirectory;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BusinessUnitMapperShould {
+
+    private BusinessUnitMapper mapper;
+    private BusinessUnit  businessUnitInput;
+    private BusinessUnitResponse expectedOutput;
+
+    @Before
+    public void setup(){
+        mapper = new BusinessUnitMapper();
+        businessUnitInput = new BusinessUnit("Services");
+        businessUnitInput.setId(1);
+        expectedOutput = new BusinessUnitResponse(1,"Services");
+    }
+
+    @Test
+    public void KeepSameId(){
+        Assert.assertEquals(businessUnitInput.getId(), expectedOutput.getId());
+    }
+
+    @Test
+    public void KeepSameNameOfBusinessUnit(){
+        Assert.assertEquals(businessUnitInput.getNameOfBusinessUnit(), expectedOutput.getNameOfBusinessUnit());
+    }
+
+    @Test
+    public void mapBusinessinessUnitToResponse(){
+        BusinessUnitResponse output = mapper.mapBusinessinessUnitToResponse(businessUnitInput);
+        Assert.assertThat(expectedOutput, Matchers.samePropertyValuesAs(output));
+    }
+}
