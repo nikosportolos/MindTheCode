fd6833660a7a9826e22a8a7db42285e16ea226a5
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-18 16:34:29 +0200

    Added user prompt on app exit

--- Java Spring Boot Project/company_directory_app/lib/screens/home_screen.dart
+++ Java Spring Boot Project/company_directory_app/lib/screens/home_screen.dart
@@ -23,58 +23,101 @@ class _HomePageState extends State<MyHomePage> {
 
   _HomePageState(this._title);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(right: 10),
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  height: 50,
-                  width: 50,
+  Future<bool> _onWillPop() {
+    return showDialog(
+          context: context,
+          builder: (context) => new AlertDialog(
+            title: Row(
+              children: <Widget>[
+                Padding(
+                  child: Icon(
+                    Icons.exit_to_app,
+                    color: Colors.blue,
+                  ),
+                  padding: EdgeInsets.only(right: 5.0),
+                ),
+                Text("Exit"),
+              ],
+            ),
+            content: new Text('Do you really want to exit $_title?'),
+            actions: <Widget>[
+              new FlatButton(
+                color: Colors.blue,
+                onPressed: () => Navigator.of(context).pop(false),
+                child: new Text(
+                  'No',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-              Text(this._title)
+              new FlatButton(
+                color: Colors.blue,
+                onPressed: () => Navigator.of(context).pop(true),
+                child: new Text(
+                  'Yes',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
             ],
           ),
-        ),
-        body: ListView(
-          children: <Widget>[
-            HomeTileWidget(
-              title: 'Companies',
-              icon: Icon(Icons.account_balance),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CompaniesScreen())),
+        ) ??
+        false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    height: 50,
+                    width: 50,
+                  ),
+                ),
+                Text(this._title)
+              ],
             ),
-            HomeTileWidget(
-              title: 'Business Units',
-              icon: Icon(Icons.business),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => BusinessUnitsScreen())),
-            ),
-            HomeTileWidget(
-              title: 'Departments',
-              icon: Icon(Icons.store_mall_directory),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => DepartmentsScreen())),
-            ),
-            HomeTileWidget(
-              title: 'Units',
-              icon: Icon(Icons.streetview),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => UnitsScreen())),
-            ),
-            HomeTileWidget(
-              title: 'Positions',
-              icon: Icon(Icons.label),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => PositionsScreen())),
-            ),
-            HomeTileWidget(
-              title: 'Employees',
-              icon: Icon(Icons.people),
-              onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => EmployeesScreen())),
-            ),
-          ],
-        ));
+          ),
+          body: ListView(
+            children: <Widget>[
+              HomeTileWidget(
+                title: 'Companies',
+                icon: Icon(Icons.account_balance),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => CompaniesScreen())),
+              ),
+              HomeTileWidget(
+                title: 'Business Units',
+                icon: Icon(Icons.business),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => BusinessUnitsScreen())),
+              ),
+              HomeTileWidget(
+                title: 'Departments',
+                icon: Icon(Icons.store_mall_directory),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => DepartmentsScreen())),
+              ),
+              HomeTileWidget(
+                title: 'Units',
+                icon: Icon(Icons.streetview),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => UnitsScreen())),
+              ),
+              HomeTileWidget(
+                title: 'Positions',
+                icon: Icon(Icons.label),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => PositionsScreen())),
+              ),
+              HomeTileWidget(
+                title: 'Employees',
+                icon: Icon(Icons.people),
+                onTapCallback: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => EmployeesScreen())),
+              ),
+            ],
+          )),
+    );
   }
 }
