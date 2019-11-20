ac67c9616b6543a2ff46ddc272f725d4824aba68
dimitrafountzoula <dimfou109@gmail.com>
2019-11-21 00:22:29 +0200

    AddBusinessUnit/AddBusinessUnits

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/controllers/BusinessUnitController.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/controllers/BusinessUnitController.java
@@ -1,13 +1,13 @@
 package com.mindthecode.CompanyDirectory.controllers;
 
+import com.mindthecode.CompanyDirectory.models.entities.BusinessUnit;
+import com.mindthecode.CompanyDirectory.models.entities.Employee;
 import com.mindthecode.CompanyDirectory.models.responses.ErrorResponse;
 import com.mindthecode.CompanyDirectory.services.BusinessUnitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BusinessUnitController {
@@ -31,6 +31,38 @@ public class BusinessUnitController {
         try {
             System.out.println("###Loading business unit by id: " + id);
             return new ResponseEntity<>(service.getBusinessUnitById(id), null, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new ErrorResponse(0, "Error", "Something went wrong"), null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addBusinessUnit")
+    @ResponseBody
+    public ResponseEntity addBusinessUnit(@RequestBody BusinessUnit newBusinessUnit) {
+        try {
+            System.out.println("###Adding new businessUnit: " + newBusinessUnit.toString());
+            var response = service.saveBusinessUnit(newBusinessUnit);
+            if(response.getError() == null)
+                return new ResponseEntity<>(response, null, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(response.getError(), null, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new ErrorResponse(0, "Error", "Something went wrong"), null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addBusinessUnits")
+    @ResponseBody
+    public ResponseEntity addBusinessUnits(@RequestBody Iterable<BusinessUnit> newBusinessUnits) {
+        try {
+            System.out.println("###Adding multiple new businessUnits");
+            var response = service.saveBusinessUnits(newBusinessUnits);
+            if(response.getError() == null)
+                return new ResponseEntity<>(response, null, HttpStatus.OK);
+            else
+                return new ResponseEntity<>(response.getError(), null, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(new ErrorResponse(0, "Error", "Something went wrong"), null, HttpStatus.INTERNAL_SERVER_ERROR);
