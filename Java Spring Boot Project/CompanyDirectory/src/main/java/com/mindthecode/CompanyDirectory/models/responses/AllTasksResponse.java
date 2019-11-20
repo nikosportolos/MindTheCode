4d9f58a7044b1a5e99a84a4a4c5cb0e8a9c9e911
fvkontos <fvkontos@hotmail.com>
2019-11-20 17:49:24 +0200

    changed AllTasksResponse class and created a TaskResponse class

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/models/responses/AllTasksResponse.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/models/responses/AllTasksResponse.java
@@ -1,70 +1,24 @@
 package com.mindthecode.CompanyDirectory.models.responses;
 
-import com.mindthecode.CompanyDirectory.models.entities.TaskStatus;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AllTasksResponse
 {
-    private long taskId;
-    private String title;
-    private String description;
-    private int difficulty;
-    private TaskStatus status;
+    private List<TaskResponse> tasks;
 
     public AllTasksResponse() {
     }
 
-    public AllTasksResponse(long taskId, String title, String description, int difficulty, TaskStatus status) {
-        this.taskId = taskId;
-        this.title = title;
-        this.description = description;
-        this.difficulty = difficulty;
-        this.status = status;
+    public AllTasksResponse(List<TaskResponse> tasks) {
+        this.tasks = tasks;
     }
 
-    public AllTasksResponse(String title, String description, int difficulty, TaskStatus status) {
-        this.title = title;
-        this.description = description;
-        this.difficulty = difficulty;
-        this.status = status;
+    public List<TaskResponse> getTasks() {
+        return tasks;
     }
 
-    public long getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(long taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getDifficulty() {
-        return difficulty;
-    }
-
-    public void setDifficulty(int difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
+    public void setTasks(List<TaskResponse> tasks) {
+        this.tasks = tasks;
     }
 }
