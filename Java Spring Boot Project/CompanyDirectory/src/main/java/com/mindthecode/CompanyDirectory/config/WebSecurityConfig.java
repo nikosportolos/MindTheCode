2b1945ec6cec87b6ca6a03fa4768d1bc0c79658b
Nikos Portolos <nikos.portolos@gmail.com>
2019-11-21 23:09:38 +0200

    Modified WebSecurityConfig class - changed configuration

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/config/WebSecurityConfig.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/config/WebSecurityConfig.java
@@ -4,7 +4,10 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,8 +42,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .headers()
                 .frameOptions().sameOrigin()
+
                 .and()
                 .authorizeRequests()
 
@@ -71,65 +76,63 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/getTasksByDiffAndNumOfEmployees  ").permitAll()
 
                 // Delete single entity
-                .antMatchers("/deleteCompany/").permitAll()
-                .antMatchers("/deleteBusinessUnit/").permitAll()
-                .antMatchers("/deleteDepartment/").permitAll()
-                .antMatchers("/deleteUnit/").permitAll()
-                .antMatchers("/deletePosition/").permitAll()
-                .antMatchers("/deleteEmployee/").permitAll()
-                .antMatchers("/deleteTask/").permitAll()
+                .antMatchers("/deleteCompany/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteBusinessUnit/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteDepartment/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteUnit/").hasAnyRole("ADMIN")
+                .antMatchers("/deletePosition/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteEmployee/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteTask/").hasAnyRole("ADMIN")
 
                 // Delete multiple entities
-                .antMatchers("/deleteCompanies/").permitAll()
-                .antMatchers("/deleteBusinessUnits/").permitAll()
-                .antMatchers("/deleteDepartments/").permitAll()
-                .antMatchers("/deleteUnits/").permitAll()
-                .antMatchers("/deletePositions/").permitAll()
-                .antMatchers("/deleteEmployees/").permitAll()
-                .antMatchers("/deleteTasks/").permitAll()
+                .antMatchers("/deleteCompanies/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteBusinessUnits/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteDepartments/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteUnits/").hasAnyRole("ADMIN")
+                .antMatchers("/deletePositions/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteEmployees/").hasAnyRole("ADMIN")
+                .antMatchers("/deleteTasks/").hasAnyRole("ADMIN")
 
                 // Add single entity
-                .antMatchers("/addCompany/").permitAll()
-                .antMatchers("/addBusinessUnit").permitAll()
-                .antMatchers("/addDepartment/").permitAll()
-                .antMatchers("/addUnit/").permitAll()
-                .antMatchers("/addPosition/").permitAll()
-                .antMatchers("/addEmployee/").permitAll()
-                .antMatchers("/addTask/").permitAll()
+                .antMatchers("/addCompany/").hasAnyRole("ADMIN")
+                .antMatchers("/addBusinessUnit").hasAnyRole("ADMIN")
+                .antMatchers("/addDepartment/").hasAnyRole("ADMIN")
+                .antMatchers("/addUnit/").hasAnyRole("ADMIN")
+                .antMatchers("/addPosition/").hasAnyRole("ADMIN")
+                .antMatchers("/addEmployee/").hasAnyRole("ADMIN")
+                .antMatchers("/addTask/").hasAnyRole("ADMIN")
 
                 // Add multiple entities
-                .antMatchers("/addCompanies/").permitAll()
-                .antMatchers("/addBusinessUnits").permitAll()
-                .antMatchers("/addDepartments/").permitAll()
-                .antMatchers("/addUnits/").permitAll()
-                .antMatchers("/addPositions/").permitAll()
-                .antMatchers("/addEmployees/").permitAll()
-                .antMatchers("/addTasks/").permitAll()
+                .antMatchers("/addCompanies/").hasAnyRole("ADMIN")
+                .antMatchers("/addBusinessUnits").hasAnyRole("ADMIN")
+                .antMatchers("/addDepartments/").hasAnyRole("ADMIN")
+                .antMatchers("/addUnits/").hasAnyRole("ADMIN")
+                .antMatchers("/addPositions/").hasAnyRole("ADMIN")
+                .antMatchers("/addEmployees/").hasAnyRole("ADMIN")
+                .antMatchers("/addTasks/").hasAnyRole("ADMIN")
 
                 // Update single entity
-                .antMatchers("/updateCompany/").permitAll()
-                .antMatchers("/updateBusinessUnit/").permitAll()
-                .antMatchers("/updateDepartment/").permitAll()
-                .antMatchers("/updateUnit/").permitAll()
-                .antMatchers("/updatePosition/").permitAll()
-                .antMatchers("/updateEmployee/").permitAll()
-                .antMatchers("/updateTask/").permitAll()
+                .antMatchers("/updateCompany/").hasAnyRole("ADMIN")
+                .antMatchers("/updateBusinessUnit/").hasAnyRole("ADMIN")
+                .antMatchers("/updateDepartment/").hasAnyRole("ADMIN")
+                .antMatchers("/updateUnit/").hasAnyRole("ADMIN")
+                .antMatchers("/updatePosition/").hasAnyRole("ADMIN")
+                .antMatchers("/updateEmployee/").hasAnyRole("ADMIN")
+                .antMatchers("/updateTask/").hasAnyRole("ADMIN")
 
                 // Update multiple entities
-                .antMatchers("/updateCompanies/").permitAll()
-                .antMatchers("/updateBusinessUnits/").permitAll()
-                .antMatchers("/updateDepartments/").permitAll()
-                .antMatchers("/updateUnits/").permitAll()
-                .antMatchers("/updatePositions/").permitAll()
-                .antMatchers("/updateEmployees/").permitAll()
-                .antMatchers("/updateTasks/").permitAll()
+                .antMatchers("/updateCompanies/").hasAnyRole("ADMIN")
+                .antMatchers("/updateBusinessUnits/").hasAnyRole("ADMIN")
+                .antMatchers("/updateDepartments/").hasAnyRole("ADMIN")
+                .antMatchers("/updateUnits/").hasAnyRole("ADMIN")
+                .antMatchers("/updatePositions/").hasAnyRole("ADMIN")
+                .antMatchers("/updateEmployees/").hasAnyRole("ADMIN")
+                .antMatchers("/updateTasks/").hasAnyRole("ADMIN")
 
-                .anyRequest().permitAll()
-//                .anyRequest().authenticated()
+                .anyRequest().authenticated()
 
                 .and()
                 .formLogin()
-                // .loginPage("/login")
                 .defaultSuccessUrl("/home")
                 .failureUrl("/login?error")
                 .permitAll()
@@ -141,74 +144,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .deleteCookies("my-remember-me-cookie")
                 .permitAll()
 
+//                .and()
+//                .rememberMe()
+//                //.key("my-secure-key")
+//                .rememberMeCookieName("my-remember-me-cookie")
+//                .tokenRepository(persistentTokenRepository())
+//                .tokenValiditySeconds(24 * 60 * 60)
+
                 .and()
-                .rememberMe()
-                //.key("my-secure-key")
-                .rememberMeCookieName("my-remember-me-cookie")
-                .tokenRepository(persistentTokenRepository())
-                .tokenValiditySeconds(24 * 60 * 60)
-
-                /**
-                 *** Temporary authentication bypass
-                 */
-                .and().csrf().ignoringAntMatchers("/h2-console/**")
-                // Delete single entity
-                .and().csrf().ignoringAntMatchers("/deleteCompany/")
-                .and().csrf().ignoringAntMatchers("/deleteBusinessUnit/")
-                .and().csrf().ignoringAntMatchers("/deleteDepartment/")
-                .and().csrf().ignoringAntMatchers("/deleteUnit/")
-                .and().csrf().ignoringAntMatchers("/deletePosition/")
-                .and().csrf().ignoringAntMatchers("/deleteEmployee/")
-                .and().csrf().ignoringAntMatchers("/deleteTask/")
-
-                // Delete multiple entities
-                .and().csrf().ignoringAntMatchers("/deleteCompanies/")
-                .and().csrf().ignoringAntMatchers("/deleteBusinessUnits/")
-                .and().csrf().ignoringAntMatchers("/deleteDepartments/")
-                .and().csrf().ignoringAntMatchers("/deleteUnits/")
-                .and().csrf().ignoringAntMatchers("/deletePositions/")
-                .and().csrf().ignoringAntMatchers("/deleteEmployees/")
-                .and().csrf().ignoringAntMatchers("/deleteTasks/")
-
-                // Add single entity
-                .and().csrf().ignoringAntMatchers("/addCompany/")
-                .and().csrf().ignoringAntMatchers("/addBusinessUnit")
-                .and().csrf().ignoringAntMatchers("/addDepartment/")
-                .and().csrf().ignoringAntMatchers("/addUnit/")
-                .and().csrf().ignoringAntMatchers("/addPosition/")
-                .and().csrf().ignoringAntMatchers("/addEmployee/")
-                .and().csrf().ignoringAntMatchers("/addTask/")
-
-                // Add multiple entities
-                .and().csrf().ignoringAntMatchers("/addCompanies/")
-                .and().csrf().ignoringAntMatchers("/addBusinessUnits")
-                .and().csrf().ignoringAntMatchers("/addDepartments/")
-                .and().csrf().ignoringAntMatchers("/addUnits/")
-                .and().csrf().ignoringAntMatchers("/addPositions/")
-                .and().csrf().ignoringAntMatchers("/addEmployees/")
-                .and().csrf().ignoringAntMatchers("/addTasks/")
-
-                // Update single entity
-                .and().csrf().ignoringAntMatchers("/updateCompany/")
-                .and().csrf().ignoringAntMatchers("/updateBusinessUnit/")
-                .and().csrf().ignoringAntMatchers("/updateDepartment/")
-                .and().csrf().ignoringAntMatchers("/updateUnit/")
-                .and().csrf().ignoringAntMatchers("/updatePosition/")
-                .and().csrf().ignoringAntMatchers("/updateEmployee/")
-                .and().csrf().ignoringAntMatchers("/updateTask/")
-
-                // Update multiple entities
-                .and().csrf().ignoringAntMatchers("/updateCompanies/")
-                .and().csrf().ignoringAntMatchers("/updateBusinessUnits/")
-                .and().csrf().ignoringAntMatchers("/updateDepartments/")
-                .and().csrf().ignoringAntMatchers("/updateUnits/")
-                .and().csrf().ignoringAntMatchers("/updatePositions/")
-                .and().csrf().ignoringAntMatchers("/updateEmployees/")
-                .and().csrf().ignoringAntMatchers("/updateTasks/")
-                /**
-                 * End of temp fix
-                 */
-
+                .httpBasic()
                 .and()
                 .exceptionHandling()
         ;
