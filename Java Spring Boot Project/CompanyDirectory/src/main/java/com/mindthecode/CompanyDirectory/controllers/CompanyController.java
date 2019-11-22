64254929c2af4ccc6cf7644ffd05026b132eded4
fvkontos <fvkontos@hotmail.com>
2019-11-22 13:02:47 +0200

    changes on the controller and services with delete and deleteAll

--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/controllers/CompanyController.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/controllers/CompanyController.java
@@ -1,5 +1,6 @@
 package com.mindthecode.CompanyDirectory.controllers;
 
+import com.mindthecode.CompanyDirectory.models.entities.Company;
 import com.mindthecode.CompanyDirectory.models.responses.CompanyResponse;
 import com.mindthecode.CompanyDirectory.models.responses.ErrorResponse;
 import com.mindthecode.CompanyDirectory.models.responses.AllCompaniesResponse;
@@ -7,10 +8,7 @@ import com.mindthecode.CompanyDirectory.services.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +35,21 @@ public class CompanyController { //this is a comment
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity(new ErrorResponse(0, "Error", "Something went wrong"), null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/addCompany")
+    @ResponseBody
+    public ResponseEntity addCompany(@RequestBody Company company)
+    {
+        try{
+            System.out.println("Adding company" + company.toString() );
+            return new ResponseEntity(companyService.saveCompany(company),null,HttpStatus.OK);
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity(new ErrorResponse(0,"Error","You did not add any company"),null,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/services/CompanyService.java
+++ Java Spring Boot Project/CompanyDirectory/src/main/java/com/mindthecode/CompanyDirectory/services/CompanyService.java
@@ -2,6 +2,7 @@ package com.mindthecode.CompanyDirectory.services;
 
 import com.mindthecode.CompanyDirectory.mappers.CompanyMapper;
 import com.mindthecode.CompanyDirectory.models.entities.Company;
+import com.mindthecode.CompanyDirectory.models.responses.AllCompaniesResponse;
 import com.mindthecode.CompanyDirectory.models.responses.CompanyResponse;
 import com.mindthecode.CompanyDirectory.models.responses.ErrorResponse;
 import com.mindthecode.CompanyDirectory.models.responses.GenericResponse;
@@ -42,4 +43,18 @@ public class CompanyService {
         }
         return new GenericResponse<>(companiesToReturn);
     }
+
+    public GenericResponse<AllCompaniesResponse> saveCompany (Company company)
+    {
+        companyRepository.save(company);
+        List<CompanyResponse> retrievedCompany = new ArrayList<>();
+        retrievedCompany.add(companyMapper.mapCompanyResponseFromCompany(company));
+        return new GenericResponse<>(new AllCompaniesResponse(retrievedCompany));
+    }
+
+   /* public GenericResponse<AllCompaniesResponse> saveCompanies(Iterable<Company> companies)
+    {
+        companyRepository.saveAll(companies);
+        return new GenericResponse<>(new AllCompaniesResponse(companyMapper.mapCompanyResponseFromCompany(companies)));
+    } */
 }
