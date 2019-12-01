45cccb502d3dea28c266d78c94038d42adc29669
Nikos Portolos <nikos.portolos@gmail.com>
2019-12-01 15:00:12 +0200

    Created entity models

---
+++ .NET Core Project/Timesheet/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheet.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public User DepartmentHead { get; set; }
+    }
+}
