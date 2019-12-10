610bb1c719be1cb30f6d05206eca97a6652dbe73
Nikos Portolos <nikos.portolos@gmail.com>
2019-12-10 11:38:32 +0200

    Created ViewModels for entities: Department, Project & User

---
+++ .NET Core Project/Timesheet/Models/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timesheet.Models.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string DepartmentHeadId { get; set; }
+    }
+}
