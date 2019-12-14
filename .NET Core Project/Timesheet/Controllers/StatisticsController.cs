584d859a7dc34ae56eb47764a1b1fc2f1ac6df8e
Nikos Portolos <nikos.portolos@gmail.com>
2019-12-14 21:02:21 +0200

    Modified Home view, navbar & Statistics

--- .NET Core Project/Timesheet/Controllers/StatisticsController.cs
+++ .NET Core Project/Timesheet/Controllers/StatisticsController.cs
@@ -10,9 +10,15 @@ namespace Timesheet.Controllers
 {
     public class StatisticsController : Controller
     {
-     //   GET: Statistics
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult ProjectCostChart()
+        {
+
             Random rnd = new Random();
             var lstModel = new List<SimpleReportViewModel>();
             lstModel.Add(new SimpleReportViewModel
@@ -35,82 +41,20 @@ namespace Timesheet.Controllers
                 DimensionOne = "Water",
                 Quantity = rnd.Next(10)
             });
+
             return View(lstModel);
         }
 
-        // GET: Statistics/Details/5
-        public ActionResult Details(int id)
+        public IActionResult ProjectTimeChart()
         {
             return View();
         }
 
-        // GET: Statistics/Create
-        public ActionResult Create()
+
+        public IActionResult ProjectOverviewChart()
         {
             return View();
         }
 
-        // POST: Statistics/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Statistics/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Statistics/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Statistics/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Statistics/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
