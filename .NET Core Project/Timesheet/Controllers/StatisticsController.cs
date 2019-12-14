519afb93ae1c87edbef216c66c31e38f18127a1e
Nikoletta <n.michopoulou@hotmail.com>
2019-12-14 21:51:07 +0200

    Ποσοστό χρόνου που έχει δαπανηθεί ανά project ή/και ανά τμήμα (Pie Chart)

--- .NET Core Project/Timesheet/Controllers/StatisticsController.cs
+++ .NET Core Project/Timesheet/Controllers/StatisticsController.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Timesheet.Mappers;
+using Timesheet.Models.Entities;
 using Timesheet.Models.StatisticsModel;
+using Timesheet.Repositories;
 
 namespace Timesheet.Controllers
 {
     public class StatisticsController : Controller
     {
+        private readonly ITimesheetEntryRepository _timesheetEntryRepository;
+        private readonly IProjectRepository _projectRepository;
+        private readonly IDepartmentRepository _departmentRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ITimesheetEntryMapper _mapper;
+        private readonly UserManager<User> _userManager;
 
+        public StatisticsController(ITimesheetEntryRepository timesheetEntryRepository, ITimesheetEntryMapper mapper, UserManager<User> userManager, IProjectRepository projectRepository, IUserRepository userRepository, IDepartmentRepository departmentRepository)
+        {
+            _timesheetEntryRepository = timesheetEntryRepository;
+            _projectRepository = projectRepository;
+            _userRepository = userRepository;
+            _departmentRepository = departmentRepository;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
         public ActionResult Index()
         {
             return View();
@@ -45,9 +64,41 @@ namespace Timesheet.Controllers
             return View(lstModel);
         }
 
-        public IActionResult ProjectTimeChart()
+        public async Task<IActionResult> ProjectTimeChartAsync()
         {
-            return View();
+            /*Add hours of timesheets in each project they belong*/
+            var lstModel = new List<SimpleReportViewModel>();
+            int cost = 0;
+            List<TimesheetEntry> entries = (await _timesheetEntryRepository.GetAll()).ToList();
+            List<Project> projects = (await _projectRepository.GetAll()).ToList();
+            foreach(var x in projects)
+            {
+                foreach(var y in entries)
+                {
+                    if(y.ProjectId == x.Id)
+                    {
+                        cost += y.HoursWorked;
+                    }
+                }
+                lstModel.Add(new SimpleReportViewModel
+                {
+                    DimensionOne = x.Name,
+                    Quantity = cost
+                });
+                cost = 0;
+            }
+
+            /*
+            Random rnd = new Random();
+            
+            lstModel.Add(new SimpleReportViewModel
+            {
+                DimensionOne = "Beer",
+                Quantity = rnd.Next(10)
+            });
+            */
+
+            return View(lstModel);           
         }
 
 
